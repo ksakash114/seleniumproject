112a310c6456717ebd2bc4468e81697e1d6a2463
ksakash114 <ksakash114@gmail.com>
2025-03-16 19:17:49 +0530

    changes

---
+++ src/main/java/selenium/pracproject/example1.java
@@ -0,0 +1,11 @@
+package selenium.pracproject;
+
+public class example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		System.out.println("abcd");
+	}
+
+}
